2d51f17affd0a3928ff70b651989fd81de18c1c7
Sangwoo Han <realappsdev@gmail.com>
2022-09-13 22:45:08 +0900

    fix: eda

--- src/eda.py
+++ src/eda.py
@@ -5,9 +5,10 @@ Created on 2022/09/12
 import os
 import random
 import re
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 import joblib
+import numpy as np
 
 
 def _get_only_hangul(line: str) -> str:
@@ -15,96 +16,87 @@ def _get_only_hangul(line: str) -> str:
     return parsed_text
 
 
+def _get_synonyms(word: str, wordnet: Dict[str, List[str]]) -> List[str]:
+    return wordnet[word] if word in wordnet else []
+
+
 ########################################################################
 # Synonym replacement
 # Replace n words in the sentence with synonyms from wordnet
 ########################################################################
 def _synonym_replacement(
-    wordnet: Dict[str, List[str]], words: List[str], n: int
-) -> List[str]:
-    new_words = words.copy()
-    random_word_list = list(set([word for word in words]))
+    sentence: str, wordnet: Dict[str, List[str]], n: int
+) -> Optional[str]:
+    words = sentence.strip().split(" ")
+
+    random_word_list = list(set(words))
     random.shuffle(random_word_list)
     num_replaced = 0
+
     for random_word in random_word_list:
-        synonyms = _get_synonyms(wordnet, random_word)
+        synonyms = _get_synonyms(random_word, wordnet)
+
         if len(synonyms) >= 1:
-            synonym = random.choice(list(synonyms))
-            new_words = [synonym if word == random_word else word for word in new_words]
+            synonym = random.choice(synonyms)
+            words = [synonym if word == random_word else word for word in words]
             num_replaced += 1
+
         if num_replaced >= n:
             break
 
-    if len(new_words) != 0:
-        sentence = " ".join(new_words)
-        new_words = sentence.split(" ")
-
-    else:
-        new_words = ""
-
-    return new_words
-
-
-def _get_synonyms(wordnet: Dict[str, List[str]], word: str) -> List[str]:
-    synomyms = []
-
-    try:
-        for syn in wordnet[word]:
-            for s in syn:
-                synomyms.append(s)
-    except:
-        pass
-
-    return synomyms
+    return " ".join(words) if num_replaced == 0 else None
 
 
 ########################################################################
 # Random deletion
 # Randomly delete words from the sentence with probability p
 ########################################################################
-def _random_deletion(words: List[str], p: float) -> List[str]:
-    if len(words) == 1:
-        return words
+def _random_deletion(sentence: str, p: float) -> Optional[str]:
+    words = sentence.strip().split(" ")
 
     new_words = []
     for word in words:
-        r = random.uniform(0, 1)
-        if r > p:
+        if np.random.binomial(1, p) == 0:
             new_words.append(word)
 
     if len(new_words) == 0:
-        rand_int = random.randint(0, len(words) - 1)
-        return [words[rand_int]]
+        return None
 
-    return new_words
+    return " ".join(new_words)
 
 
 ########################################################################
 # Random swap
 # Randomly swap two words in the sentence n times
 ########################################################################
-def _random_swap(words: List[str], n: int) -> List[str]:
-    new_words = words.copy()
+def _random_swap(sentence: str, n: int) -> Optional[str]:
+    new_words = sentence.strip().split(" ")
+
+    if len(new_words) == 1:
+        return None
+
     for _ in range(n):
         new_words = _swap_word(new_words)
 
-    return new_words
+    return " ".join(new_words)
 
 
-def _swap_word(new_words: List[str]) -> List[str]:
-    random_idx_1 = random.randint(0, len(new_words) - 1)
-    random_idx_2 = random_idx_1
+def _swap_word(words: List[str]) -> List[str]:
+    new_words = words.copy()
+
+    ridx_1 = np.random.randint(len(new_words))
+    ridx_2 = ridx_1
+
     counter = 0
-
-    while random_idx_2 == random_idx_1:
-        random_idx_2 = random.randint(0, len(new_words) - 1)
+    while ridx_2 == ridx_1:
+        ridx_2 = np.random.randint(len(new_words))
         counter += 1
         if counter > 3:
-            return new_words
+            return words
 
-    new_words[random_idx_1], new_words[random_idx_2] = (
-        new_words[random_idx_2],
-        new_words[random_idx_1],
+    new_words[ridx_1], new_words[ridx_2] = (
+        new_words[ridx_2],
+        new_words[ridx_1],
     )
     return new_words
 
@@ -114,38 +106,41 @@ def _swap_word(new_words: List[str]) -> List[str]:
 # Randomly insert n words into the sentence
 ########################################################################
 def _random_insertion(
-    wordnet: Dict[str, List[str]], words: List[str], n: int
-) -> List[str]:
-    new_words = words.copy()
+    sentence: str, wordnet: Dict[str, List[str]], n: int
+) -> Optional[str]:
+    new_words = sentence.strip().split(" ")
+    added = []
     for _ in range(n):
-        _add_word(wordnet, new_words)
+        added.append(_add_word(new_words, wordnet))
 
-    return new_words
+    return " ".join(new_words) if any(added) else None
 
 
-def _add_word(wordnet: Dict[str, List[str]], new_words: List[str]) -> None:
+def _add_word(words: List[str], wordnet: Dict[str, List[str]]) -> bool:
     synonyms = []
     counter = 0
     while len(synonyms) < 1:
-        if len(new_words) >= 1:
-            random_word = new_words[random.randint(0, len(new_words) - 1)]
-            synonyms = _get_synonyms(wordnet, random_word)
-            counter += 1
-        else:
-            random_word = ""
+        random_word = words[np.random.randint(len(words))]
+        synonyms = _get_synonyms(random_word, wordnet)
+        counter += 1
+        if counter > 3:
+            return False
 
-        if counter >= 10:
-            return
+    random_synonym = synonyms[np.random.randint(len(synonyms))]
+    ridx = np.random.randint(len(words))
+    words.insert(ridx, random_synonym)
 
-    random_synonym = synonyms[0]
-    random_idx = random.randint(0, len(new_words) - 1)
-    new_words.insert(random_idx, random_synonym)
+    return True
 
 
 class EDA:
     def __init__(
         self,
         root_data_dir: str = "./data",
+        p_sr: float = 0.25,
+        p_ri: float = 0.25,
+        p_rs: float = 0.25,
+        p_rd: float = 0.25,
         alpha_sr: float = 0.1,
         alpha_ri: float = 0.1,
         alpha_rs: float = 0.1,
@@ -153,6 +148,10 @@ class EDA:
         num_aug: int = 9,
     ) -> None:
         self._wordnet = joblib.load(os.path.join(root_data_dir, "wordnet"))
+        self._p_sr = p_sr
+        self._p_ri = p_ri
+        self._p_rs = p_rs
+        self._p_rd = p_rd
         self._alpha_sr = alpha_sr
         self._alpha_ri = alpha_ri
         self._alpha_rs = alpha_rs
@@ -164,48 +163,46 @@ class EDA:
         sentence: str,
     ) -> List[str]:
         sentence = _get_only_hangul(sentence)
-        words = sentence.split(" ")
-        words = [word for word in words if word is not ""]
-        num_words = len(words)
+        num_words = len(sentence.strip().split(" "))
 
         augmented_sentences = []
-        num_new_per_technique = int(self._num_aug / 4) + 1
+        num_new_per_technique = np.random.multinomial(
+            self._num_aug,
+            [self._p_sr, self._p_ri, self._p_rs, self._p_rd],
+        )
 
         n_sr = max(1, int(self._alpha_sr * num_words))
         n_ri = max(1, int(self._alpha_ri * num_words))
         n_rs = max(1, int(self._alpha_rs * num_words))
 
         # sr
-        for _ in range(num_new_per_technique):
-            a_words = _synonym_replacement(self._wordnet, words, n_sr)
-            augmented_sentences.append(" ".join(a_words))
+        for _ in range(num_new_per_technique[0]):
+            a_sent = _synonym_replacement(sentence, self._wordnet, n_sr)
+            if a_sent is not None:
+                augmented_sentences.append(a_sent)
 
         # ri
-        for _ in range(num_new_per_technique):
-            a_words = _random_insertion(self._wordnet, words, n_ri)
-            augmented_sentences.append(" ".join(a_words))
+        for _ in range(num_new_per_technique[1]):
+            a_sent = _random_insertion(sentence, self._wordnet, n_ri)
+            if a_sent is not None:
+                augmented_sentences.append(a_sent)
 
         # rs
-        for _ in range(num_new_per_technique):
-            a_words = _random_swap(words, n_rs)
-            augmented_sentences.append(" ".join(a_words))
+        for _ in range(num_new_per_technique[2]):
+            a_sent = _random_swap(sentence, n_rs)
+            if a_sent is not None:
+                augmented_sentences.append(a_sent)
 
         # rd
-        for _ in range(num_new_per_technique):
-            a_words = _random_deletion(words, self._alpha_rd)
-            augmented_sentences.append(" ".join(a_words))
+        for _ in range(num_new_per_technique[3]):
+            a_sent = _random_deletion(sentence, self._alpha_rd)
+            if a_sent is not None:
+                augmented_sentences.append(a_sent)
 
-        augmented_sentences = [
-            _get_only_hangul(sentence) for sentence in augmented_sentences
-        ]
+        if len(augmented_sentences) == 0:
+            augmented_sentences.append(sentence)
+
+        augmented_sentences = list(set(augmented_sentences))
         random.shuffle(augmented_sentences)
-
-        if self._num_aug >= 1:
-            augmented_sentences = augmented_sentences[: self._num_aug]
-        else:
-            keep_prob = self._num_aug / len(augmented_sentences)
-            augmented_sentences = [
-                s for s in augmented_sentences if random.uniform(0, 1) < keep_prob
-            ]
 
         return augmented_sentences
